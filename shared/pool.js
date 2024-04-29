8ec3128a9777d771bdc46a03095af09272994e18
maxximzz <maxime.fp77@gmail.com>
2024-04-29 13:57:40 -0400

    server

---
+++ shared/pool.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root1234',
+  database: 'estore',
+  port: 3306,
+  multipleStatements: true,
+});
+
+module.exports = pool;
